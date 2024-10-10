38b8e6055998a3270c744a25c56dcc58391d21ac
chandra_fajar <chandra@stellardata.ai>
2024-10-10 11:05:35 +0700

    fix bug

--- src/features/Dashboard/CardInformationDashboard.jsx
+++ src/features/Dashboard/CardInformationDashboard.jsx
@@ -2,8 +2,35 @@ import React from "react";
 import { Card, Col, Row } from "react-bootstrap";
 import { DFlexColumn, DFlexJustifyBetween } from "../../styled/styled.flex";
 import styled from "styled-components";
+import axiosInstance from "../../config/axios/axiosInstance";
+import { useQuery } from "react-query";
+import formatRupiah from "../../config/formatRupiah";
 
 export default function CardInformationDashboard() {
+  const fetchMenu = async (page = 1) => {
+    const res = await axiosInstance.get(`/api/v1/menu/get?page=${page}`);
+    return res.data;
+  };
+
+  const fetchOrder = async (page = 1) => {
+    const res = await axiosInstance.get(`/api/v1/order/get?page=${page}`);
+    return res.data;
+  };
+
+  const { data: dataOrder } = useQuery(["orders"], () => fetchOrder(), {
+    keepPreviousData: true,
+  });
+  const {
+    data: dataMenu,
+    isLoading,
+    error,
+  } = useQuery(["menus"], () => fetchMenu(), {
+    keepPreviousData: true,
+  });
+
+  //total revenue
+  const totalRevenue = dataOrder?.orders?.reduce((total, order) => total + order.totalPrice, 0);
+
   return (
     <Row>
       <Col xl={"4"} md={"6"} className="mb-4">
@@ -11,7 +38,7 @@ export default function CardInformationDashboard() {
           <DFlexJustifyBetween>
             <DFlexColumn style={{ gap: "0" }}>
               <TitleSpan>Total Menus</TitleSpan>
-              <TitleAngka>3,248</TitleAngka>
+              <TitleAngka>{dataMenu?.menu?.length}</TitleAngka>
             </DFlexColumn>
             <img src="/images/icons/book-square.svg" alt="" />
           </DFlexJustifyBetween>
@@ -22,7 +49,7 @@ export default function CardInformationDashboard() {
           <DFlexJustifyBetween>
             <DFlexColumn style={{ gap: "0" }}>
               <TitleSpan>Total Transaksi</TitleSpan>
-              <TitleAngka>3,248</TitleAngka>
+              <TitleAngka>{dataOrder?.orders?.length}</TitleAngka>
             </DFlexColumn>
             <img src="/images/icons/coin.svg" alt="" />
           </DFlexJustifyBetween>
@@ -33,7 +60,7 @@ export default function CardInformationDashboard() {
           <DFlexJustifyBetween>
             <DFlexColumn style={{ gap: "0" }}>
               <TitleSpan>Total Revenue</TitleSpan>
-              <TitleAngka>3,248</TitleAngka>
+              <TitleAngka>{formatRupiah(totalRevenue)}</TitleAngka>
             </DFlexColumn>
             <img src="/images/icons/cards.svg" alt="" />
           </DFlexJustifyBetween>
@@ -51,7 +78,7 @@ const TitleSpan = styled.span`
 `;
 
 const TitleAngka = styled.h2`
-  font-size: 2.5rem;
+  font-size: 2rem;
   font-style: normal;
   font-weight: 600;
   line-height: normal;
